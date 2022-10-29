2e15e2936e3049944628666dffe71dbb4de68d3d
jiakun <jiakun_wang@yeah.net>
2022-10-29 13:35:04 +0000

    固化：不需要修改切片的函数，直接使用切片控制块作为参数

--- unicode切片.h
+++ unicode切片.h
@@ -5,15 +5,19 @@
     #include <stdint.h>
     #include <stdio.h>
     #include <stdlib.h>
-    // unicode 只读 静态串结构，使用 变长数组
-    typedef struct {
+    
+    typedef struct {// unicode只读静态串结构，使用 变长数组
         uint16_t 长度;
-        uint16_t 容量;
-        unicode字符 静态串[0];
+        unicode字符 数据[0];
     } unicode静态串;
     
-    // unicode切片结构体，支持最多6,5536个字符，参考go切片、rust切片等切片设计
-    typedef struct {
+    typedef struct {// unicode连续动态串结构，使用 变长数组
+        uint16_t 长度;
+        uint16_t 容量;
+        unicode字符 数据[0];
+    } unicode动态串;
+    
+    typedef struct {// unicode切片结构体，支持最多6,5536个字符，参考go切片、rust切片等切片设计
         uint16_t 长度;
         uint16_t 容量;
         unicode字符 * 数据;
@@ -26,8 +30,8 @@
     void unicode切片_扩容(unicode切片 * 切片指针, int 期望容量);  //√ 已测试
     void unicode切片_追加(unicode切片 * 切片指针, unicode字符 待追加字符); //√ 已测试
     unicode切片 unicode切片_复制(unicode切片 * 源切片);
-    void unicode切片_文件打印(unicode切片 * 切片指针, FILE * 输出文件); //√ 已测试
-    void unicode切片_文件序列化打印(unicode切片 * 切片指针, FILE * 输出文件); //√ 已测试
+    void unicode切片_文件打印(unicode切片 切片, FILE * 输出文件); //√ 已测试
+    void unicode切片_文件序列化打印(unicode切片 切片, FILE * 输出文件); //√ 已测试
     // 以下为 纯函数
     bool unicode切片_判等(unicode切片 * 被比对切片, unicode切片 比对切片); //√ 已测试
     int64_t unicode切片_转整数(unicode切片 * 切片指针, uint8_t 进制); //√ 已测试
