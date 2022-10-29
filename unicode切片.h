181fe121f25fd65730e119ab2ca34eddbdb0cc1d
jiakun <jiakun_wang@yeah.net>
2022-10-29 14:46:36 +0000

    调整：不会更改切片的方法，直接传控制块

--- unicode切片.h
+++ unicode切片.h
@@ -29,11 +29,11 @@
     void unicode切片_重置(unicode切片 * 切片指针); //√ 已测试
     void unicode切片_扩容(unicode切片 * 切片指针, int 期望容量);  //√ 已测试
     void unicode切片_追加(unicode切片 * 切片指针, unicode字符 待追加字符); //√ 已测试
-    unicode切片 unicode切片_复制(unicode切片 * 源切片);
+    unicode切片 unicode切片_复制(unicode切片 源切片);
     void unicode切片_文件打印(unicode切片 切片, FILE * 输出文件); //√ 已测试
     void unicode切片_文件序列化打印(unicode切片 切片, FILE * 输出文件); //√ 已测试
     // 以下为 纯函数
-    bool unicode切片_判等(unicode切片 * 被比对切片, unicode切片 比对切片); //√ 已测试
-    int64_t unicode切片_转整数(unicode切片 * 切片指针, uint8_t 进制); //√ 已测试
-    uint32_t unicode切片_计算散列值(unicode切片 * 切片指针, uint32_t 最大值); //√ 已测试
+    bool unicode切片_判等(unicode切片 被比对切片, unicode切片 比对切片); //√ 已测试
+    int64_t unicode切片_转整数(unicode切片 切片, uint8_t 进制); //√ 已测试
+    uint32_t unicode切片_计算散列值(unicode切片 切片, uint32_t 模); //√ 已测试
 #endif
